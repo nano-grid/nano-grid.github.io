bae2ea40431b4b70e0b12362e1eb34cb8278a878
Miguel <jemiguelrivas@gmail.com>
2022-05-31 08:03:41 -0400

    configuring web components

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  lintOnSave: false,
+
+  devServer: {
+    host: 'mr',
+    port: 8200
+  },
+
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        options['compilerOptions'] = {
+          ...options.compilerOptions || {},
+          isCustomElement: tag => tag.toLowerCase().indexOf("nn-") === 0
+        };
+        return options;
+      })
+  }
+}
